f3fc25a2ecdb1028b791dbc70343d22854ce7b7d
Lars Nielsen <lnc13.lars@gmail.com>
2017-11-06 23:32:35 +0100

    notebook and automatic config generation

---
+++ generate_config.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import math
+
+one_kb = 1024
+one_mb = one_kb * one_kb
+ten_mb = 10 * one_mb
+twenty_mb = 2 * ten_mb
+one_gb = 1024 * one_mb
+two_gb = 2 * one_gb
+
+sizes = [one_kb, one_mb, ten_mb, twenty_mb, one_gb, two_gb]
+PATH = './configs/'
+def write_to_config(config, number, generation_size):
+    config_file = '{}{}-config-{}'.format(PATH,generation_size, number)
+    f = open(config_file, 'w')
+    f.write(config)
+    f.close()
+
+
+
+def create_config(data_size, generation_size, itterations,
+                  finite_field, redundancy):
+
+    symbol_size = math.floor(data_size / generation_size)
+    return """generation_size:{}
+symbol_size:{}
+redundancy:{}
+finite_field:{}
+itterations:{}""".format(generation_size, symbol_size, redundancy,
+                                      finite_field, itterations)
+
+def main():
+    generation_size = int(input('Generation Size: '))
+    finite_field = input('finite_field(1,2,3): ')
+    itterations = input('itterations(std=10k): ')
+    redundancy = input('redundancy(std=0): ')
+
+    if len(itterations) == 0:
+        itterations = 10000
+    if len(redundancy) == 0:
+        redundancy = 0
+
+    x = 1
+    for i in sizes:
+        config = create_config(i, generation_size, itterations,
+                  finite_field, redundancy)
+        write_to_config(config, x, generation_size)
+        x = x + 1
+
+
+
+
+if __name__ == "__main__":
+    main()
