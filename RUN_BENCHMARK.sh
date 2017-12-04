90f0356d4ba08c756e9f0502723160d34eb2a839
Lars Nielsen <lnc13.lars@gmail.com>
2017-12-04 09:13:38 +0100

    benchmarks

--- RUN_BENCHMARK.sh
+++ RUN_BENCHMARK.sh
@@ -1,1 +1,13 @@
-./build/darwin/full_vector_benchmark configs/config-1 results/
+#!/usr/bin/env bash
+
+sh actual_on_the_fly_encoder_benchmark.sh
+sleep 600
+sh full_vector_encoder_benchmark.sh
+sleep 600
+sh on_the_fly_encoder_benchmark.sh
+sleep 600
+sh perpetual_encoder_benchmark.sh
+sleep 600
+sh shallow_full_vector_encoder_benchmark.sh
+sleep 600
+sh shallow_perpetual_encoder_benchmark.sh
---
+++ full_vector_encoder_benchmark.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+##########################################################################
+#                                                                        #
+# This file contains the executin instructions for the Full Vector       #
+# benchmark.                                                             #
+#                                                                        #
+##########################################################################
+
+##########################################################################
+#                                                                        #
+# Generation size 8 benchmark                                            #
+#                                                                        #
+##########################################################################
+
+./build/linux/full_vector_encoder_benchmark configs/8-config-1 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/8-config-2 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/8-config-3 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/8-config-4 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/8-config-5 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/8-config-6 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/8-config-7 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/8-config-8 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/8-config-9 ./results/encoder/thousand/
+sleep 300
+
+##########################################################################
+#                                                                        #
+# Generation size 16 benchmark                                           #
+#                                                                        #
+##########################################################################
+
+./build/linux/full_vector_encoder_benchmark configs/16-config-1 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/16-config-2 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/16-config-3 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/16-config-4 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/16-config-5 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/16-config-6 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/16-config-7 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/16-config-8 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/16-config-9 ./results/encoder/thousand/
+sleep 300
+
+##########################################################################
+#                                                                        #
+# Generation size 32 benchmark                                           #
+#                                                                        #
+##########################################################################
+
+./build/linux/full_vector_encoder_benchmark configs/32-config-1 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/32-config-2 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/32-config-3 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/32-config-4 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/32-config-5 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/32-config-6 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/32-config-7 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/32-config-8 ./results/encoder/thousand/
+sleep 300
+./build/linux/full_vector_encoder_benchmark configs/32-config-9 ./results/encoder/thousand/
